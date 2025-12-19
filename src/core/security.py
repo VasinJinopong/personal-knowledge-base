985cfc9553bd2b60518328855f84514e84a5e231
Vasin Jinopong <vasinearth14@gmail.com>
2025-12-19 20:59:07 +0700

    Excluded security for  docs,redoc

--- src/core/security.py
+++ src/core/security.py
@@ -1,11 +1,18 @@
-from fastapi import Security, HTTPException
+from fastapi import Security, HTTPException, Request
 from fastapi.security import APIKeyHeader
 from src.core.config import get_settings
 
 
 api_key_header = APIKeyHeader(name="X-API-Key")
 
-def verify_api_key(api_key: str = Security(api_key_header)):
+
+EXCLUDE_PATHS = ["/docs", "/redoc", "/openapi.json"]
+
+def verify_api_key(request : Request,api_key: str = Security(api_key_header)):
+    
+    if request.url.path in EXCLUDE_PATHS:
+        return
+    
     if api_key != get_settings.API_KEY:
         raise HTTPException(status_code=403, detail="Invalid API Key")
     
